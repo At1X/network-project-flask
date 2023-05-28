f393f5ed60f2e2bc4bd4acba37a4e7a63c2c63c9
At1X <amirmahdijafarian@gmail.com>
2023-05-28 12:00:14 +0430

    posts & fix db initial creation make post creation and receive all of them or 1b1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 import psycopg2
 import hashlib
-
+from psycopg2 import sql
+import sys
 
 app = Flask(__name__)
 
@@ -25,6 +26,29 @@ def connect_db():
     return conn
 
 
+def create_database():
+    conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user='postgres',
+            password='Atid8431'
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            sql.SQL("CREATE DATABASE {}")
+            .format(sql.Identifier('flask_db'))
+        )
+    except:
+        return
+
+    cursor.close()
+    conn.close()
+
+create_database()
+
 
 def table_exists(table_name):
     """Checks if the specified table exists in the database."""
@@ -47,8 +71,11 @@ if not table_exists('users') and not table_exists('posts'):
                 'password varchar NOT NULL)'
                 )
     
+    conn.commit()
+
     cur.execute('CREATE TABLE posts (id serial PRIMARY KEY, '
                 'user_id integer NOT NULL REFERENCES users (id), '
+                'post_name varchar (255) NOT NULL, '
                 'content text NOT NULL);'
                 )
     
@@ -109,7 +136,86 @@ def login():
             return {"message": "Invalid username or password.", "error": 1}
 
 
+@app.route('/create_post', methods=["POST"])
+def create_post():
+    if request.method == "POST":
+        user_id = request.json['user_id']
+        post_name = request.json['post_name']
+        content = request.json['content']
 
+        conn = connect_db()
+        cur = conn.cursor()
+
+        cur.execute('INSERT INTO posts (user_id, post_name, content) '
+                    'VALUES (%s, %s, %s)',
+                    (user_id, post_name, content)
+                    )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return {"message": "Post created successfully.", "error": 0}
+
+
+@app.route('/get_posts', methods=["GET"])
+def get_posts():
+    if request.method == "GET":
+        conn = connect_db()
+        cur = conn.cursor()
+
+        cur.execute('SELECT * FROM posts')
+        rows = cur.fetchall()
+
+        posts = []
+        for row in rows:
+            posts.append({"id": row[0], "user_id": row[1], "name": row[2], "content": row[3]})
+
+        cur.close()
+        conn.close()
+
+        return {"posts": posts, "error": 0}
+
+
+@app.route('/get_posts_by_user', methods=["GET"])
+def get_posts_by_user():
+    if request.method == "GET":
+        app.logger.debug(request.args)
+        user_id = request.args.get('user_id')
+
+        conn = connect_db()
+        cur = conn.cursor()
+
+        cur.execute('SELECT * FROM posts WHERE user_id = %s', (user_id,))
+        rows = cur.fetchall()
+
+        posts = []
+        for row in rows:
+            posts.append({"id": row[0], "user_id": row[1], "name": row[2], "content": row[3]})
+
+        cur.close()
+        conn.close()
+
+        return {"posts": posts, "error": 0}
+    
+
+@app.route('/users_count', methods=["GET"])
+def get_count_users():
+    if request.method == "GET":
+        conn = connect_db()
+        cur = conn.cursor()
+
+        cur.execute('SELECT COUNT(*) FROM users')
+        rows = cur.fetchall()
+
+        # app.logger.debug(rows)
+        users = 0
+        for row in rows:
+            users = row[0]
+
+        cur.close()
+        conn.close()
+
+        return {"users_count": users, "error": 0}
 
 
 if __name__ == '__main__':
